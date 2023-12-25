3f4b381ce7c68dd0a60ac086697aadbb043ff393
Pucho <martinezochandarenam@gmail.com>
2023-12-25 13:58:04 +0100

    peticion con fetch a graphQL sin cliente

--- graphQl-react-apollo/src/App.tsx
+++ graphQl-react-apollo/src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+  useEffect(() => {
+    fetch('https://countries.trevorblades.com/graphql', { // ------>  se especefica la url del servidor de graphQl al que se quiere hacer la peticion
+      method:'POST', //-----> el metodo es fijo siempre, este es el unico que admite QL
+      headers: {'Content-Type': 'application/json'}, // -----> se especifica el tipo de cabecera
+      body:JSON.stringify({ query:`query {
+        continents {
+          code
+          name
+        }
+      }`
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      console.log("est es la respuesta que me da QL -------->", res.data)
+    })
+    .catch((e) => {
+      console.log("este es el error ----->", e)
+    })
+  })
 
   return (
     <>
@@ -17,7 +37,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +47,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
